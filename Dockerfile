c55b95bd95d7a3a58edd6c8395acdf3e2bf33c5f
Antonio Lopes <antoniolopesg@protonmail.com>
2022-10-21 16:44:23 -0300

    add nodejs application image example

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+from node:lts
+
+COPY . .
+
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+function handle(_request, response) {
+  response.write('Hello World');
+  response.end();
+}
+
+const server = http.createServer(handle);
+
+server.listen(80)
